83748267430e19726ec7fe14289e10d638e4ec3d
Perkles <nadavex@gmail.com>
2025-09-18 22:24:52 -0500

    Atualizando Pacientes

---
+++ Atendimento.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+// Criando classe //
+
+public class Atendimento {
+    public long id;
+    public Date date;
+    public Date hora;
+    public Double valor;
+    public String status;
+    public String observacao;
+}
