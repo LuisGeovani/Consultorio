cf5d2ca8b7c40931169a5b1d95a5eb2f5c684789
Lucir Vaz <sofisma.vaz@gmail.com>
2025-09-19 17:32:25 -0500

    feat:criaProfissional Incluir a função que cria os atributos

---
+++ Profissional.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+// Criando classe //
+
+public class Atendimento {
+    public long id;
+    public String nome;
+    public String registroConselho;
+    public String telefone;
+    public String email;
+}
+
+    public void Unidade(long id){
+        return id;
+    }
+    public long Especialidade(long id){
+        return id;
+    }
