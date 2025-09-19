170d360314d745291dab5c41be085087beb2f09a
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-18 20:22:25 -0500

    Classes: Convenio, Especialidade e Unidade implementadas

---
+++ Convenio.java
@@ -0,0 +1,10 @@
+public class Convenio {
+    public Long id; 
+    public String nome; 
+    public String razaoSocial; 
+    public String cnpj; 
+    public String representante; 
+    public String email; 
+    public String telefone; 
+    public boolean ativo; 
+}
---
+++ Unidade.java
@@ -0,0 +1,5 @@
+public class Unidade {
+    public String id;
+    public String nome;
+    public String endereco;
+}
