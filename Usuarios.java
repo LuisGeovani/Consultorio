f67300a590252d33eb4e4c0d2d9c5bdbbc720dc9
ClaraSerena <claraserena03@gmail.com>
2025-09-19 19:08:51 -0500

    usarios getters e setters

--- Usuarios.java
+++ Usuarios.java
@@ -1,8 +1,49 @@
 public class Usuarios {
-    public Long id;
-    public String nomeCompleto;
-    public String nomeUsuario;
-    public String senha;
-    public Boolean ativo;
-    public String papel;
+    private Long id;
+    private String nomeCompleto;
+    private String nomeUsuario;
+    private String senha;
+    private Boolean ativo;
+    private String papel;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+        
+    }
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public Boolean getAtivo() {
+        return ativo;
+    }
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+    public String getPapel() {
+        return papel;
+    }
+    public void setPapel(String papel) {
+        this.papel = papel;
+    } 
+
 }
+
+
